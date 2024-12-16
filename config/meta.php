dfb2cc0d726b6d627723ee2acd2387a38adc74e9
Yasir Turk <yasirmturk@gmail.com>
2024-12-16 18:41:12 +0100

    Fixes env vars social and email

---
+++ config/meta.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'support' => [
+        'email' => env('SUPPORT_EMAIL', 'mail@zabrdast.com'),
+        'admin' => env('ADMIN_EMAIL', 'mail@zabrdast.com'),
+    ],
+    'social' => [
+        'facebook' => env('SOCIAL_FACEBOOK', 'https://zabrdast.com'),
+        'twitter' => env('SOCIAL_TWITTER', 'https://zabrdast.com'),
+        'instagram' => env('SOCIAL_INSTA', 'https://zabrdast.com'),
+        'linkedin' => env('SOCIAL_LINKEDIN', 'https://zabrdast.com'),
+    ]
+];
