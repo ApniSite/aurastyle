e141ff4e9c5fe35f8d8e88ce69ddd9e58c500ce7
Yasir Turk <yasirmturk@gmail.com>
2025-01-12 04:02:13 +0100

    Updates collection, brand, promo images

--- resources/views/livewire/brand-page.blade.php
+++ resources/views/livewire/brand-page.blade.php
@@ -1,9 +1,12 @@
 <x-slot name="title">{{ $this->title . ' | Tops, Sweaters, Cardigans & more | ' . config('app.name') }}</x-slot>
 <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 lg:px-8">
     <section>
-        <h1 class="text-3xl font-bold">
-            {{ $this->brand->name }}
-        </h1>
+        <div class="flex items-center gap-4">
+            <img class="p-2 w-16 h-16 rounded-full bg-gray-500 hover:bg-indigo-700" src="{{ $this->brand->thumbnail?->getUrl() }}" alt="{{ $this->brand->name }}">
+            <div>
+                <h2 class="text-3xl font-bold">{{ $this->brand->name }}</h2>
+            </div>
+        </div>
 
         <div class="grid grid-cols-2 gap-8 my-4 sm:grid-cols-3 lg:grid-cols-4">
             @forelse($this->products as $product)
