d04720284d00014463121f13ec6b6429a095f05d
Yasir Turk <yasirmturk@gmail.com>
2025-01-13 00:08:24 +0100

    Updates promo links

--- resources/views/components/promo-banner.blade.php
+++ resources/views/components/promo-banner.blade.php
@@ -1,7 +1,7 @@
 <section  {{ $attributes }}>
   <div class="max-w-screen-xl mx-auto grid grid-cols-2 sm:grid-cols-9 gap-2 md:gap-2.5 mx-auto text-white">
     <!-- Row 1: Boxes -->
-    <a href="{{ route('collection.view', ['slug' => 'men']) }}" class="group overflow-hidden col-span-2 sm:col-span-5 text-center">
+    <a href="{{ route('collection.view', ['slug' => 'ladies']) }}" class="group overflow-hidden col-span-2 sm:col-span-5 text-center">
     <span>
         <img alt="Women dress sale" src="{{ asset('images/banner-women-sale-2025.webp') }}" alt="" class="transition-transform duration-300 group-hover:scale-105">
     </span>
@@ -11,7 +11,7 @@
         <img alt="Register for 50% discount" src="{{ asset('images/banner-discount-2025.webp') }}" class="min-h-full transition-transform duration-300 group-hover:scale-105">
     </div>
     </a>
-    <a href="{{ route('collection.view', ['slug' => 'ladies']) }}" class="group overflow-hidden col-span-1 sm:col-span-2 text-center">
+    <a href="{{ route('collection.view', ['slug' => 'men']) }}" class="group overflow-hidden col-span-1 sm:col-span-2 text-center">
     <span>
         <img alt="Men's Collection" src="{{ asset('images/banner-men-2025.webp') }}" class="min-h-full transition-transform duration-300 group-hover:scale-105" >
     </span>
@@ -27,7 +27,7 @@
         <video class="min-h-full object-cover" autoplay loop muted><source src="{{ asset('images/banner-new-discount-50.mp4') }}" type="video/mp4" />New Discount 50%</video>
     </span>
     </a>
-    <a href="#box6" class="flex group overflow-hidden col-span-2 sm:col-span-5 text-center">
+    <a href="{{ route('register') }}" class="flex group overflow-hidden col-span-2 sm:col-span-5 text-center">
     <div class="m-auto py-2">
     <h1 class="text-black text-3xl font-extrabold sm:text-5xl">
         Welcome to
